3f4025d1793ca47a0013d1e11d5f48eccf39b696
COMP202_Mahyar_Bayran <mahyar.bayran@mail.mcgill.ca>
2018-11-18 16:37:12 -0500

    first push!

--- main.py
+++ main.py
@@ -19,3 +19,46 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 import numpy as np 
 
+
+'''
+this seems to run really slow
+used a simple dictionary instead
+
+class Character:
+    
+    def __init__(self, name, firstline):
+        self.name = name 
+        self.listOfLines = []
+        self.addToLines(firstline)
+        
+    def addToLines(self, newline):
+        self.listOfLines.append(newline) #a list of lists
+'''
+
+file_corpus = open('corpus.txt')
+corpus = file_corpus.readlines()
+
+# a list of character names
+charNames = []
+Characters = {}
+
+for line in corpus:
+    a = line.index(':')
+    name = line[0:a]
+    sent = line[a+2:-1]
+    sent = word_tokenize(sent.lower().strip())
+    sent.append('\n') # indicate the end of the sentence
+    
+    if name not in charNames:
+        charNames.append(name)
+        #newChar = Character(name, sent)
+        #Characters[name] = newChar
+        Characters[name] = []
+    #else:
+        #Characters[name].addToLines(sent)
+
+    Characters[name].append(sent)
+
+
+for sent in Characters['Denny']:
+    print(sent)
