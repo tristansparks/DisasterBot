5a5b80e39e701844fe88d8fcf269bdb96c6605dd
COMP202_Mahyar_Bayran <mahyar.bayran@mail.mcgill.ca>
2018-11-19 13:08:00 -0500

    Markov done, but not efficiently

--- main.py
+++ main.py
@@ -18,22 +18,69 @@ from sklearn import svm
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 import numpy as np 
+import os.path
 
 
-'''
-this seems to run really slow
-used a simple dictionary instead
+class MarkovModel:
 
-class Character:
+    def __init__(self, name, listOfLines):
+        self.name = name
+        self.states = {} #a mapping from the word to number (indice of the word in states vector)
+        count = 0
+        # create mapping for states
+        for line in listOfLines:
+            for w in line:
+                if w not in list(self.states.keys()): #new word found
+                    self.states[w] = count
+                    count += 1
+                    
+        # with mappings we can easily build initial distributions and transition matrix
+
+        #### Lots of them have zero probabilities... so why keep them? (NOT DONE!!!!!!)
+        #### For transition, keep a list of next possible states and then count and normalize
+        #### For Initial_dist, keep a list of possible first words and then cound and normalize
+        self.transition = np.zeros( (count,count) ) # Transition probabilities
+        self.initial_dist = np.zeros( count ) # same length as states
+        
+        for line in listOfLines:
+            # for now set the initial_dist by the first words in each LINE, not sentences
+            self.initial_dist[self.states[line[0]]] += 1
+            for i in list(range(0,len(line)-1)):
+                self.transition[ self.states[line[i]], self.states[line[i+1]] ] += 1
+        self.initial_dist /= self.initial_dist.sum()
+        for i in list(range(0, len(self.transition)-1)):
+            self.transition[i] /= self.transition[i].sum() 
     
+    def write_info(self, file):
+        file.write('States: \n'+ str(list(self.states.items())) +'\n\n\n')
+        file.write('Initial Distributions: \n' + str(self.initial_dist.tolist()) + '\n\n\n')
+        file.write('Transition Probabilities: \n' + str(self.transition.tolist() )+ '\n\n\n')
+        
+        
+    #def generate(self, length):
+        
+    
+class Character:
+
     def __init__(self, name, firstline):
         self.name = name 
         self.listOfLines = []
-        self.addToLines(firstline)
+        self.MM =[]
         
     def addToLines(self, newline):
         self.listOfLines.append(newline) #a list of lists
-'''
+
+    def BuildAMarkovModel(self):
+        self.MM = MarkovModel(self.name, self.listOfLines)
+
+    def write_info(self):
+        filename = "%s.txt"%self.name
+        completeName = os.path.join(os.getcwd() + '\\Characters', filename)
+        file = open(completeName, "w")
+        file.write("Character's Name:\t%s\n\n\n"%self.name)
+        self.MM.write_info(file)
+        
+######### PRE-PROCESSING ###################
 
 file_corpus = open('corpus.txt')
 corpus = file_corpus.readlines()
@@ -42,23 +89,33 @@ corpus = file_corpus.readlines()
 charNames = []
 Characters = {}
 
+# read lines and create new characters
 for line in corpus:
     a = line.index(':')
     name = line[0:a]
-    sent = line[a+2:-1]
+    sent = line[a+2:] # -1 or nothing?
     sent = word_tokenize(sent.lower().strip())
     sent.append('\n') # indicate the end of the sentence
     
     if name not in charNames:
         charNames.append(name)
-        #newChar = Character(name, sent)
-        #Characters[name] = newChar
-        Characters[name] = []
-    #else:
-        #Characters[name].addToLines(sent)
+        newChar = Character(name, sent)
+        Characters[name] = newChar
 
-    Characters[name].append(sent)
+    Characters[name].addToLines(sent)
+
+#for sent in Characters['Denny']:
+#    print(sent)
+
+############## Markov Model ###########
+
+for name in charNames:
+    Characters[name].BuildAMarkovModel()
+    Characters[name].write_info()
 
 
-for sent in Characters['Denny']:
-    print(sent)
+
+
+
+
+
