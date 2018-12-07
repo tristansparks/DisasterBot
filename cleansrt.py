d4d40a2af66980df8bf99c733557da118f3d6f6a
Mahyar_Bayran <mahyar.bayran@mail.mcgill.ca>
2018-12-07 00:07:25 -0500

    srts!

---
+++ cleansrt.py
@@ -0,0 +1,31 @@
+import pysrt
+import os
+import string
+
+# Loading the Subtitle
+names = os.listdir('srts')
+count = 1;
+
+for name in names:
+    subs = pysrt.open('srts/'+name, encoding='iso-8859-1')
+    file = open('cleanedSRT/'+name[:-4]+'.txt', 'w')
+
+    subs.stream
+    text = subs.text
+    text = text.lower().split('\n')[1:]
+    text = list(filter(lambda x: ('[' not in x) and ('<' not in x) and ('@' not in x) and ('(' not in x) and ('{' not in x), text))
+    txt = ""
+    
+    for line in text:
+        line.encode("ascii", "ignore")
+        line = list(filter(lambda x: ord(x) < 128, line))
+        line = ''.join(str(e) for e in line)
+        if line.startswith('-'):
+            txt += (line[1:].strip())
+        else:
+            txt += (line)
+        txt += " "
+    txt = txt.replace("''",'"')
+    
+    file.write(txt)
+    file.close()
