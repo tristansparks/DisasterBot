ffbdce44dacaf97307f245ba7fe9b852b502ba56
Mahyar_Bayran <mahyar.bayran@mail.mcgill.ca>
2018-12-08 14:01:11 -0500

    ROUGE + preplexity

--- Classifier.py
+++ Classifier.py
@@ -4,6 +4,7 @@ Find movies that are similar in scripts to the original movie
 
 import os
 import string
+import math
 
 import nltk
 from nltk.corpus import stopwords
@@ -13,7 +14,10 @@ from nltk.collocations import BigramCollocationFinder
 from nltk.metrics import BigramAssocMeasures
 from nltk.util import ngrams
 
-
+from nltk.stem import WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer()
+    
 def ROUGE(A, B):
     # A: list of reference n-grams
     # B: list of system n-grams
@@ -23,6 +27,32 @@ def ROUGE(A, B):
             count += 1
     return count/len(A)
 
+def Perplexity(A, B):
+    # A: list of reference n-grams
+    # B: list of system n-grams
+    voc = set(A)
+    voc = voc.union(set(B))
+    p = {}
+    q = {}
+    for w in voc:
+        p[w] = 0
+        q[w] = 0
+    for gram in A:
+        p[gram] = p[gram] + 1
+    for gram in A:
+        p[gram] = p[gram] / len(A)
+    for gram in B:
+        q[gram] = q[gram] + 1
+    for gram in B:
+        q[gram] = q[gram] / len(B)
+
+    tmp = 0
+    for w in voc:
+        if ((p[w] != 0) & (q[w] != 0)):
+            tmp += p[w]*math.log2(q[w])
+
+    return 2**(-tmp)
+    
 def remove_punc(s):
 
     table = str.maketrans('', '', string.punctuation)
@@ -33,51 +63,90 @@ def get_bigrams(myString):
 
     tokenizer = WordPunctTokenizer()
     tokens = tokenizer.tokenize(myString)
-    stemmer = PorterStemmer()
     bigrams =  list(ngrams(tokens, 2)) 
-    #bigram_finder = BigramCollocationFinder.from_words(tokens)
-    #bigrams = bigram_finder.nbest(BigramAssocMeasures.chi_sq, 500)
-    
+   
     for bigram_tuple in bigrams:
         x = "%s %s" % bigram_tuple
         tokens.append(x)
     
-    result = [' '.join([stemmer.stem(w).lower() for w in x.split()]) for x in tokens if x.lower() not in stopwords.words('english') and len(x) > 8]
+    result = [' '.join([wordnet_lemmatizer.lemmatize(w).lower() for w in x.split()]) \
+              for x in tokens if x.lower() not in stopwords.words('english') and len(x) > 8]
     return result
 
-def find_similar_scripts(method):
+def get_unigrams(myString):
+
+    tokenizer = WordPunctTokenizer()
+    tokens = tokenizer.tokenize(myString)
+    
+    result = [' '.join([wordnet_lemmatizer.lemmatize(w).lower() for w in x.split()]) \
+              for x in tokens if x.lower() not in stopwords.words('english') and len(x) > 8]
+    return result
+
+def find_similar_scripts(method, n_top):
 
     ### clean the original and extract bigrams
     file = open('corpus.txt', 'r')
     corpus = file.readlines()
     file.close()
 
+    unigrams_corpus = []
     bigrams_corpus = []
     for line in corpus:
         tmp = line.find(':')
         text = line[tmp+2:].strip().lower()
         text = remove_punc(text)
-        bigrams_line = get_bigrams(text)
-        bigrams_corpus += bigrams_line
+        unigrams_corpus += get_unigrams(text)
+        bigrams_corpus += get_bigrams(text)
+        both_corpus = unigrams_corpus + bigrams_corpus
     ### load other scripts and do the same
-    chosen_ones = []
-    threshold = 0.1
     
     names = os.listdir('cleanedSRT')
-
+    scores = {}
+    sorted_scores = []
+    sorted_scores += names
+    
     for name in names:
-        print('checking: '+ name + '    ...')
+        #print('checking: '+ name + '    ...')
         file = open('cleanedSRT/'+name, 'r')
         # there is one line only
         text = file.readline()
         text = remove_punc(text) #already did in cleansrt.py
         bigrams_ex = get_bigrams(text)
-        score = ROUGE(bigrams_corpus, bigrams_ex)
-        print(score)
-        if score >= threshold:
-            chosen_ones.append(name)
-    
-    return chosen_ones
+        unigrams_ex = get_unigrams(text)
+        both_ex = unigrams_ex + bigrams_ex
+        if method == 'ROUGE':
+            #score_uni = ROUGE(unigrams_corpus, unigrams_ex)
+            #score_bi = ROUGE(bigrams_corpus, bigrams_ex)
+            score_both = ROUGE(both_corpus, both_ex)
+            scores[name] = score_both
+            print('---ROUGE Score: '+ str(score_both))
+            #if score_both >= threshold:
+            #    chosen_ones.append(name)
+        if method == 'PERPLEXITY':
+            #scores[name] = Perplexity(bigrams_corpus, bigrams_ex)
+            #scores[name] = Perplexity(unigrams_corpus, unigrams_ex)
+            scores[name] = Perplexity(both_corpus, both_ex)
+            #print('---Perplexity Score: '+ str(scores[name]))
+            
+    # sort filenames by their scores
+    for i in range(0, len(scores.keys()) -1):
+        for j in range(i+1, len(scores.keys() )):
+            if scores[sorted_scores[j]] > scores[sorted_scores[i]]:
+                tmp = sorted_scores[j]
+                sorted_scores[j] = sorted_scores[i]
+                sorted_scores[i] = tmp
 
-method = 'ROUGE'
-print(find_similar_scripts(method))
+    #for name in sorted_scores:
+    #    print(name + ' :\t' + str(scores[name]))            
+    
+    return sorted_scores[0:n_top]
+
+# choose the method
+method1 ='ROUGE'
+method2 = 'PERPLEXITY'
+n_top = 10
+
+print(' ROUGE : \n' )
+print(find_similar_scripts(method1, n_top))
+print(' Perplexity : \n' )
+print(find_similar_scripts(method2, n_top))
