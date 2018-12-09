3c6ec84c46c0e67cdf40f635f54e34ce76a3af2d
tristansparks <tristan.sparks@mail.mcgill.ca>
2018-12-09 09:30:18 -0500

    testing new model

--- MarkovModelModule.py
+++ MarkovModelModule.py
@@ -32,11 +32,11 @@ def random_choice(options, probabilities):
     for i in range(len(sorted_by_value)):
         if ( (random_num >= cumm_sorted[i]) & (random_num <= cumm_sorted[i+1]) ):
             index = i
-            print(index)
             break
 
     return sorted_by_value[index][0]
     
+# for n = 3, this takes >5 minutes and approximately 4*wordcount MB of RAM
 class MarkovModel:    
     def __init__(self, name, listOfLines, n, smooth_param=1):
         self.name = name
@@ -137,220 +137,230 @@ class MarkovModel:
             i += 1
             
         return generated_sent
-
-
-#######Both of these might be stupid########################
-class WeightedMarkovModel:
-    def __init__(self, name, listOfLines, externalCorpus):
-        # we are basically going to chuck out external data which is unrelated to our primary corpus
-        # and then weight the resulting distributions
-        self.name = name
-        self.states = {} #a mapping from the word to index of the word in probability arrays
-        self.inverseStates = {}
-        self.listOfLines = listOfLines
-        self.wordCount = 0
-        self.externalCorpus = externalCorpus
-        self.externalCounts = {}
-        self.externalBigrams = {}
-        
-        # create mapping for states
-        for line in listOfLines:
-            for w in line:
-                if w not in list(self.states.keys()): #new word found
-                    self.states[w] = self.wordCount
-                    self.wordCount += 1
-                    
-        for line in self.externalCorpus:
-            # set up bigrams
-            for i in range(len(line) - 1):
-                if (line[i], line[i + 1]) not in self.externalBigrams.keys():
-                    self.externalBigrams[(line[i], line[i + 1])] = 0
-                self.externalBigrams[(line[i], line[i + 1])] += 1
-            
-            # set up common word counts
-            for k, v in self.states.items():
-                if (k not in self.externalCounts.keys()):
-                    self.externalCounts[k] = 0
-                
-                self.externalCounts[k] += line.count(k)
-                
-        print("word count", self.wordCount)
-        self.inverseStates = {v: k for k, v in self.states.items()}
-        self.calc_initial(0.1)
-        self.calc_transition(0.1)
-        
-                    
-    def calc_initial(self, smooth_param=0, external_weight=0.5):
-        # Use weighted average for now
-        initial_dist = np.zeros( self.wordCount ) # same length as states
-        ex_initial_dist = np.zeros( self.wordCount )
-        self.initial_dist = np.zeros( self.wordCount )
-
-        for line in self.listOfLines:
-            initial_dist[self.states[line[0]]] += 1 
-            
-        used_line_count = 0
-        for k in self.states.keys():
-            c = [ line[0] for line in self.externalCorpus ].count(k)
-            ex_initial_dist[self.states[k]] = c
-            used_line_count += c
-            
-        initial_dist = np.array( [ elem + smooth_param for elem in initial_dist ] )
-        initial_dist /= ( len(initial_dist) * smooth_param + len(self.listOfLines) )
-        
-        ex_initial_dist = np.array( [ elem + smooth_param for elem in ex_initial_dist ] )
-        ex_initial_dist /= ( len(initial_dist) * smooth_param + used_line_count )
-            
-        for i in range(len(initial_dist)):
-            self.initial_dist[i] = initial_dist[i] * (1 - external_weight) + ex_initial_dist[i] * external_weight
-            
-        
-    def calc_transition(self, smooth_param=0, external_weight=0.5):
-        self.transition = [ [] for _ in range(self.wordCount) ]
-        transition = np.zeros( (self.wordCount, self.wordCount) ) # Transition probabilities
-        ex_transition = np.zeros( ( self.wordCount, self.wordCount ) )
-        
-        for line in self.listOfLines:
-            for i in range(len(line) - 1):
-                transition[ self.states[line[i]], self.states[line[i+1]] ] += 1
-
-        for i in range(len(transition)):            
-            for j in range(len(transition)):
-                # this is so gross come back and make it better
-                if (self.inverseStates[i], self.inverseStates[j]) in self.externalBigrams.keys():
-                    ex_transition[i][j] = self.externalBigrams[(self.inverseStates[i], self.inverseStates[j])]
-                else:
-                    ex_transition[i][j] = 0
-                    
-            corpus_size = transition[i].sum()
-            ex_corpus_size = ex_transition[i].sum()
-            transition[i] = [ elem + smooth_param for elem in transition[i] ]
-            transition[i] /= ( corpus_size + smooth_param * self.wordCount )
-            
-            ex_transition[i] = [ elem + smooth_param for elem in ex_transition[i] ]
-            ex_transition[i] /= ( ex_corpus_size + smooth_param * self.wordCount )
-            
-            self.transition[i] = transition[i] * (1 - external_weight) + ex_transition[i] * external_weight
-                        
-    def generate(self, word_limit=20):
-        
-        end_tokens = [".", "?", "!"]
-        absolute_max = word_limit + 20
-        
-        words = [0 for _ in range(len(self.states))]
-        
-        for elem in self.states.items():
-            words[elem[1]] = elem[0]
-    
-        first_word = np.random.choice(words, p=self.initial_dist)
-        
-        generated_sent = [first_word]
-        
-        prev = first_word
-        for _ in range(word_limit - 1):
-            probs = self.transition[self.states[prev]]
-            new = random_choice(words, probs)
-            generated_sent.append(new)
-            
-            prev = new
-            
-        while(new not in end_tokens and absolute_max > 0):
-            absolute_max -= 1
-            probs = self.transition[self.states[prev]]
-            new = random_choice(words, probs)
-            generated_sent.append(new)
-            
-            prev = new
-            
-        return generated_sent
         
 
-class ComboMarkovModel:
-    # takes two markov models and the weight of the first one
-    def __init__(self, mm1, mm2, weight):
-        self.mm1 = mm1
-        self.mm2 = mm2
+class WeightedComboMarkovModel:
+    '''
+    This model takes two markov models,
+    and combines their initial and transition probability distributions
+    INPUTS:
+        primaryMM - the primary corpus markov model
+        externalMM - the external markov model 
+        weight - the weight of distributions from the primary model
+    '''
+    def __init__(self, primaryMM, externalMM, weight):
+        if (externalMM.n != n or primaryMM.n != n):
+            print("primary and external corpora must be of same order")
+            return
+
+        self.primaryMM = primaryMM
+        self.externalMM = externalMM
+        self.n = n
         self.weight = weight
         self.states = {}
         
         index = 0
-        for word in mm1.states.keys():
+        for word in primaryMM.states.keys():
             self.states[word] = index
             index += 1
-        for word in mm2.states.keys():
+        for word in externalMM.states.keys():
             if word not in self.states.keys():
                 self.states[word] = index
                 index += 1
+
+        self.wordCount = len(self.states)
+        self.shape = [ self.wordCount for _ in range(self.n) ]
+        self.initial_dist = np.zeros(self.wordCount)
+        self.transition = np.zeros(self.shape)
+        self.calc_initial()
+        self.calc_transition()
+        
+    def calc_initial(self):
+        print()
+        
+    def calc_transition(self):        
+        print()
+                    
+    def generate(self, word_limit=20, overflow_allowed=20):
+        end_tokens = [".", "?", "!"]
+        
+        words = [0 for _ in range(len(self.states))]
+        for elem in self.states.items():
+            words[elem[1]] = elem[0]
+        
+        # how to pick seed words for n=3?
+        first_word = random_choice(words, self.initial_dist)
+        generated_sent = [first_word]
+        if (self.n == 3):
+            # for now pick second word from random line that begins with the first word
+            lines = []
+            for line in self.listOfLines:
+                if (line[0] == first_word):
+                    lines.append(line)
+                    
+            line = random.choice(lines)
+            generated_sent.append(line[1])
+        
+        for i in range(self.n - 1, word_limit):
+            if (self.n == 2):
+                probs = self.transition[self.states[generated_sent[i-1]]]
+            elif (self.n == 3):
+                probs = self.transition[self.states[generated_sent[i-2]]][self.states[generated_sent[i-1]]]
+            
+            generated_sent.append(random_choice(words, probs))
+            
+        i = word_limit
+        while(generated_sent[-1] not in end_tokens and len(generated_sent) <= word_limit + overflow_allowed):
+            if (self.n == 2):
+                probs = self.transition[self.states[generated_sent[i-1]]]
+            elif (self.n == 3):
+                probs = self.transition[self.states[generated_sent[i-2]]][self.states[generated_sent[i-1]]]
+            
+            generated_sent.append(random_choice(words, probs))
+            i += 1
+            
+        return generated_sent
+        
+class NormalizedComboMarkovModel:
+    '''
+    This model takes two Markov Models,
+    and adds the corresponding external initial and transition probabilities to 
+    the primary distributions. We then normalize using softmax
+    INPUTS:
+        primaryMM - the primary corpus markov model
+        externalMM - the external markov model 
+    '''
+    def __init__(self, primaryMM, externalMM, external_weight):
+        if (externalMM.n != primaryMM.n):
+            print("primary and external corpora must be of same order")
+            return
+        
+        self.primaryMM = primaryMM
+        self.externalMM = externalMM
+        self.external_weight = external_weight
+        self.n = self.primaryMM.n
+        self.states = self.primaryMM.states
+        self.wordCount = len(self.states)
 
         self.calc_initial()
         self.calc_transition()
         
     def calc_initial(self):
-        self.initial_dist = np.zeros(len(self.states))
-        for gram, index in self.states.items():
-            if gram in self.mm1.states.keys():
-                i1 = self.mm1.initial_dist[self.mm1.states[gram]]
-            else:
-                i1 = 0
-            if gram in self.mm2.states.keys():
-                i2 = self.mm2.initial_dist[self.mm2.states[gram]]
-            else:
-                i2 = 0  
-            
-            self.initial_dist[index] = i1 * self.weight + i2 * (1 - self.weight)
-            
+        self.initial_dist = self.primaryMM.initial_dist
         
-    def calc_transition(self):
-        self.transition = np.zeros( (len(self.states), len(self.states)) )
+        for word, index in self.states.items():
+            if (word in self.externalMM.states.keys()):
+                self.initial_dist[index] += self.external_weight * self.externalMM.initial_dist[self.externalMM.states[word]]
+         
+        # Normalize
+        print(self.initial_dist.sum())
+        self.initial_dist = softmax(self.initial_dist)
+        print(self.initial_dist.sum())
         
-        for gram1, index1 in self.states.items():
-           # print("w1: ", word1)
-            for gram2, index2 in self.states.items():
-                if (gram1 in self.mm1.states.keys() and gram2 in self.mm1.states.keys()):
-                    t1 = self.mm1.transition[self.mm1.states[gram1]][self.mm1.states[gram2]]
-                else:
-                    t1 = 0
-                if (gram1 in self.mm2.states.keys() and gram2 in self.mm2.states.keys()):
-                    t2 = self.mm2.transition[self.mm2.states[gram1]][self.mm2.states[gram2]]
-                else:
-                    t2 = 0
-                
-                
-                if (gram1 not in self.mm1.states.keys()):
-                    self.transition[index1][index2] = t2
-                elif (gram1 not in self.mm2.states.keys()):
-                    self.transition[index1][index2] = t1
-                else:
-                    self.transition[index1][index2] = t1 * self.weight + t2 * (1 - self.weight)
-                    
-    def generate(self, word_limit=20):
+    def calc_transition(self):  
+        self.transition = self.primaryMM.transition
         
+        for word1, index1 in self.states.items():
+            for word2, index2 in self.states.items():
+                # First Order Markov
+                if (self.n == 2):
+                    if (word1 in self.externalMM.states.keys() and word2 in self.externalMM.states.keys()):
+                        self.transition[index1][index2] += self.external_weight * self.externalMM.transition[self.externalMM.states[word1]][self.externalMM.states[word2]]
+                # Second Order Markov
+                elif (self.n == 3):
+                    for word3, index3 in self.states.items():
+                        if (word1 in self.externalMM.states.keys() and word2 in self.externalMM.states.keys() and word3 in self.externalMM.states.keys()):
+                            self.transition[index1][index2][index3] += self.external_weight * self.externalMM.transition[self.externalMM.states[word1]][self.externalMM.states[word2]][self.externalMM.states[word3]]
+        
+        # Normalize
+        if (self.n == 2):
+            for i in range(len(self.transition)):
+                self.transition[i] = softmax(self.transition[i])
+        elif (self.n == 3):
+            for i in range(len(self.transition)):
+                for j in range(len(self.transition)):
+                    self.transition[i][j] = softmax(self.transition[i][j])
+                          
+    def generate(self, word_limit=20, overflow_allowed=20):
         end_tokens = [".", "?", "!"]
-        absolute_max = word_limit + 20
         
-        #grams = [0 for _ in range(len(self.states))]
-        #for elem in self.states.items():
-        #    gram[elem[1]] = elem[0]
-
-        grams = list(self.states.keys())
-    
-        first_gram = random_choice(grams, self.initial_dist)
+        words = [0 for _ in range(len(self.states))]
+        for elem in self.states.items():
+            words[elem[1]] = elem[0]
         
-        generated_sent = list(first_gram)
+        # how to pick seed words for n=3?
+        first_word = random_choice(words, self.initial_dist)
+        generated_sent = [first_word]
+        if (self.n == 3):
+            # for now pick second word from random line that begins with the first word
+            lines = []
+            for line in self.listOfLines:
+                if (line[0] == first_word):
+                    lines.append(line)
+                    
+            line = random.choice(lines)
+            generated_sent.append(line[1])
         
-        prev = first_gram
-        for _ in range(word_limit):
-            probs = self.transition[self.states[prev]]
-            new = random_choice(grams, probs)
-            generated_sent.append(new[-1])          
-            prev = new
+        for i in range(self.n - 1, word_limit):
+            if (self.n == 2):
+                probs = self.transition[self.states[generated_sent[i-1]]]
+            elif (self.n == 3):
+                probs = self.transition[self.states[generated_sent[i-2]]][self.states[generated_sent[i-1]]]
             
-        while(new[-1] not in end_tokens and absolute_max > 0):
-            absolute_max -= 1
-            probs = self.transition[self.states[prev]]
-            new = random_choice(grams, probs)
-            generated_sent.append(new[-1])     
-            prev = new
+            generated_sent.append(random_choice(words, probs))
+            
+        i = word_limit
+        while(generated_sent[-1] not in end_tokens and len(generated_sent) <= word_limit + overflow_allowed):
+            if (self.n == 2):
+                probs = self.transition[self.states[generated_sent[i-1]]]
+            elif (self.n == 3):
+                probs = self.transition[self.states[generated_sent[i-2]]][self.states[generated_sent[i-1]]]
+            
+            generated_sent.append(random_choice(words, probs))
+            i += 1
             
         return generated_sent
-        
+    
+    
+def softmax(X, theta = 1.0, axis = None):
+    """
+    Compute the softmax of each element along an axis of X.
+
+    Parameters
+    ----------
+    X: ND-Array. Probably should be floats. 
+    theta (optional): float parameter, used as a multiplier
+        prior to exponentiation. Default = 1.0
+    axis (optional): axis to compute values along. Default is the 
+        first non-singleton axis.
+
+    Returns an array the same size as X. The result will sum to 1
+    along the specified axis.
+    """
+
+    # make X at least 2d
+    y = np.atleast_2d(X)
+
+    # find axis
+    if axis is None:
+        axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
+
+    # multiply y against the theta parameter, 
+    y = y * float(theta)
+
+    # subtract the max for numerical stability
+    y = y - np.expand_dims(np.max(y, axis = axis), axis)
+    
+    # exponentiate y
+    y = np.exp(y)
+
+    # take the sum along the specified axis
+    ax_sum = np.expand_dims(np.sum(y, axis = axis), axis)
+
+    # finally: divide elementwise
+    p = y / ax_sum
+
+    # flatten if X was 1D
+    if len(X.shape) == 1: p = p.flatten()
+
+    return p
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import numpy as np
 import os.path
 import string
 from CharacterModule import Character
-from MarkovModelModule import MarkovModel, WeightedMarkovModel, ComboMarkovModel
+from MarkovModelModule import MarkovModel, WeightedComboMarkovModel, NormalizedComboMarkovModel
 
 from nltk.corpus import gutenberg
 from Classifier import find_similar_scripts, Perplexity
@@ -77,8 +77,10 @@ def strip_line_no_sentences(line):
                 
     return name, sent
     
+############################# MAIN ############################################
 file_corpus = open('corpus.txt')
 corpus = file_corpus.readlines()
+file_corpus.close()
 
 # a list of character names
 charNames = []
@@ -108,65 +110,56 @@ for line in corpus:
 #    Characters[name].BuildAMarkovModel()
 #    Characters[name].write_info()
 
-external_corpus = []
-for line in external_corpus_raw:
-    new_line = []
-    for word in line:
-        if (word not in punctuation):
-            new_line.append(word.lower())
-     
-    if (new_line != []):
-        external_corpus.append(new_line)
-
 scripts = ['Goodfellas.1990.txt', 'Love.And.Other.Drugs.txt', 'Notting.Hill.1999.txt', 'The.Notebook.2004.txt', 'Eternal.Sunshine.Of.The.Spotless.Mind.txt', 'Blue.Valentine.2010.txt', 'Sweet.November.2001.txt', '500.Days.of.Summer.2009.txt', 'One.Flew.Over.the.Cuckoos.Nest.txt', 'Sliding.Doors.txt']
 
 external_raw = []
-#for script in scripts:
-#    file = open('cleanedSRT/'+script, 'r')
-#    external_raw.append(file.readlines())
-#    file.close()
+for script in scripts:
+    file = open('cleanedSRT/'+script, 'r')
+    external_raw.append(file.readlines())
+    file.close()
     
 external = []
 
 punctuation = "\"#$%&'()*+,-/:;<=>@[\]^_`{|}~"
 end_tokens = [".", "?", "!"]
 
-#for s in external_raw[0]:
-#    sentences = []
-#    
-#    # I think dividing into sentences was a mistake
-#    raw_sentences = sent_tokenize(s)
-#    for raw_sent in raw_sentences:
-#        sent = []
-#        
-#        # tokenize sentences by hand because NLTK doesn't like words like "gonna"
-#        # which will obviously be used FREQUENTLY for our purposes
-#        words = raw_sent.split(" ")
-#        table = str.maketrans('', '', punctuation)
-#        stripped = [w.translate(table) for w in words]
-#        
-#        for word in stripped:
-#            if (word not in string.punctuation):
-#                if (True in [word.strip().endswith(c) for c in end_tokens]):
-#                    end_char = word.strip()[-1]
-#                    sent.append(word.strip()[:-1].lower())
-#                    sent.append(end_char)
-#                else:
-#                    sent.append(word.strip().lower())
-#                
-#        if (sent != []):
-#            sentences.append(sent)
-#    external.append(sentences)
-#
-#all_external = []
-#
-#for e in external:
-#    for l in e:
-#        all_external.append(l)
-#        
-#ex = MarkovModel("ex", all_external, 3, 0.0000001)
-fc = MarkovModel("fc", FullCorpus.listOfLines, 3, smooth_param=0.0000001)
+for s in external_raw[0]:
+    sentences = []
+    
+    # I think dividing into sentences was a mistake
+    raw_sentences = sent_tokenize(s)
+    for raw_sent in raw_sentences:
+        sent = []
+        
+        # tokenize sentences by hand because NLTK doesn't like words like "gonna"
+        # which will obviously be used FREQUENTLY for our purposes
+        words = raw_sent.split(" ")
+        table = str.maketrans('', '', punctuation)
+        stripped = [w.translate(table) for w in words]
+        
+        for word in stripped:
+            if (word not in string.punctuation):
+                if (True in [word.strip().endswith(c) for c in end_tokens]):
+                    end_char = word.strip()[-1]
+                    sent.append(word.strip()[:-1].lower())
+                    sent.append(end_char)
+                else:
+                    sent.append(word.strip().lower())
+                
+        if (sent != []):
+            sentences.append(sent)
+    external.append(sentences)
 
+all_external = []
 
+for e in external:
+    for l in e:
+        all_external.append(l)
+        
+ex = MarkovModel("ex", all_external, n=2, smooth_param=0.00001)
+fc = MarkovModel("fc", FullCorpus.listOfLines, n=2, smooth_param=0.00001)
+
+combo = NormalizedComboMarkovModel(fc, ex, external_weight=0)
+
+print(combo.generate())
 #mm = MarkovModel('Johnny', Characters['Johnny'].listOfLines, 3, 0.00001)
-print(fc.generate())
